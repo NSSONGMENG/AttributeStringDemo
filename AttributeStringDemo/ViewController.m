b187dd6127236058c2d025ead6455b0247c9d9ae
seven.song@ixdigit.com <>
2018-02-01 17:55:36 +0800

    添加注释

--- AttributeStringDemo/ViewController.m
+++ AttributeStringDemo/ViewController.m
@@ -25,10 +25,12 @@
     IXAttributeTapLabel * label = [[IXAttributeTapLabel alloc] initWithFrame:CGRectMake(50, 100, self.view.frame.size.width - 100, 70)];
     label.backgroundColor = [UIColor whiteColor];
     label.numberOfLines = 0;
+    //文本点击回调
     label.tapBlock = ^(NSString *string) {
         NSLog(@" -- %@ --",string);
     };
     
+    //设置需要点击的字符串，并配置此字符串的样式及位置
     IXAttributeModel    * model = [IXAttributeModel new];
     model.range = [str rangeOfString:@"智汇云信"];
     model.string = @"智汇云信";
@@ -40,7 +42,7 @@
     model1.string = @"科兴科学园";
     model1.attributeDic = @{NSForegroundColorAttributeName : [UIColor blueColor]};
     
-    
+    //label内容赋值
     [label setText:str attributes:@{NSForegroundColorAttributeName:[UIColor lightGrayColor],NSFontAttributeName:[UIFont systemFontOfSize:15]}
     tapStringArray:@[model,model1]];
     
